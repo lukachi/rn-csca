91c5dd1edbcd773070d51145eff2ecd99049eeb2
lukachi <lukachisama@gmail.com>
2025-07-04 23:27:38 +0300

    refactor: configuration

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,12 @@
 import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'rn-csca';
+import { csca_parser } from 'rn-csca';
 
-const result = multiply(3, 7);
+console.log({ csca_parser });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Result: {''}</Text>
     </View>
   );
 }
